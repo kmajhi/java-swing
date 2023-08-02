3935fe57b5a274ee2779645c80ef72fd113c1e95
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-02 20:36:40 +0600

    1. showMessageDialog.java

---
+++ 1. showMessageDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("You will see something new today.");
+        JOptionPane.showMessageDialog(null, "Ohh ho, something went wrong. But you entered correct password. Thanks anyway");
+    }
+}
