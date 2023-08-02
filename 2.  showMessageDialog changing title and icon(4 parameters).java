f3d6543da61d4fb442ed6f355ae9b44ea84be283
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-02 20:56:59 +0600

    2.  showMessageDialog changing title and icon(4 parameters).java

---
+++ 2.  showMessageDialog changing title and icon(4 parameters).java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Ohh no, wrong password.","Warning",JOptionPane.ERROR_MESSAGE);
+    }
+}
