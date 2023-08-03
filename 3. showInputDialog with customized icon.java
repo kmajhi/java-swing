75e9ee3af09d7779947efafc34fa06c68eacdd55
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-03 12:39:14 +0600

    3. showInputDialog with customized icon.java

--- 3. showInputDialog with customized icon.java
+++ 3. showInputDialog with customized icon.java
@@ -3,8 +3,14 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Main {
+    private static ImageIcon resizeImage(ImageIcon originalIcon, int width, int height){
+        Image image = originalIcon.getImage();
+        Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(resizedImage);
+    }
     public static void main(String[] args) {
-        ImageIcon originalImg = new ImageIcon("C:\\ image location");
+        
+        ImageIcon originalImg = new ImageIcon("C:\\ *.png");
         ImageIcon resizedImg = resizeImage(originalImg, 128, 128);
 
         Object[] message_1 = {resizedImg, "Enter your first name: "};
@@ -16,9 +22,4 @@ public class Main {
         JOptionPane.showMessageDialog(null, "Welcome to Jeff Association "+ name, "Jeff Association", JOptionPane.INFORMATION_MESSAGE,resizedImg);
     }
 
-    private static ImageIcon resizeImage(ImageIcon originalIcon, int width, int height) {
-        Image image = originalIcon.getImage();
-        Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        return new ImageIcon(resizedImage);
-    }
 }
