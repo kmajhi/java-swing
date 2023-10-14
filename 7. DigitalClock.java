ca3149d68a33347ef530ff9a8926456767e178dc
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-14 23:59:04 +0600

    7. DigitalClock.java

---
+++ 7. DigitalClock.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DigitalClock {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Digital Clock");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 100);
+        frame.setLayout(new FlowLayout());
+
+        JLabel timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Arial Black", Font.PLAIN, 24));
+        frame.add(timeLabel);
+
+        frame.setVisible(true);
+
+        Timer timer = new Timer(1000, e -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            Date now = new Date();
+            String timeStr = sdf.format(now);
+            timeLabel.setText(timeStr);
+        });
+        timer.start();
+    }
+}
